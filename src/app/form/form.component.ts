08b2fd854d048ad3884ff711034450682327e801
Akissohje esther <estherakissohe176@gmail.com>
2024-08-14 21:30:58 +0200

    update password verification

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,7 +10,8 @@ export class FormComponent {
   
     formPass= new FormGroup({
       email: new FormControl(''),
-      password: new FormControl('', [ Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]
+      password: new FormControl('', [Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/)]
        ),
     })
 }
+// Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
